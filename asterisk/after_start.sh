7e369b2df2795505ab1931601226092fc9f4a388
Sergey Dmitriev <serge.dmitriev@gmail.com>
2015-12-09 18:00:24 +0700

    change etc/asterisk

--- asterisk/after_start.sh
+++ asterisk/after_start.sh
@@ -1,11 +1,5 @@
 #!/bin/bash
 
-ln -s /tmp/lua-dialplan/ /usr/local/lib/lua/5.1/dialplan
-
-rm -f -v /etc/asterisk/extensions.lua
-
-ln -s /tmp/lua-dialplan/extensions.lua /etc/asterisk/extensions.lua
-
 asterisk
 
 sleep 20
--- store/etc/asterisk/extensions.lua
+++ store/etc/asterisk/extensions.lua
@@ -1,1 +1,53 @@
-/tmp/lua-dialplan/extensions.lua
+
+local dial = function (context, extension)
+    app.noop("context: " .. context .. ", extension: " .. extension);
+    app.dial('SIP/' .. extension, 10);
+    
+    local dialstatus = channel["DIALSTATUS"]:get();
+    app.noop('dialstatus: '..dialstatus);
+    app.set("CHANNEL(language)=ru");
+
+    if dialstatus == 'BUSY' then
+        app.playback("followme/sorry");        
+    elseif dialstatus == 'CHANUNAVAIL' then 
+        app.playback("followme/sorry");
+    end;
+
+    app.hangup();
+end;
+
+local ivr = function (context, extension)        
+    app.read("IVR_CHOOSE", "/var/menu/demo", 1, nil, 2, 3);
+    local choose = channel["IVR_CHOOSE"]:get();
+
+    if choose == '1' then
+        app.queue('1234');
+    elseif choose == '2' then
+        dial('internal', '101');
+    else
+        app.hangup();
+    end;
+end;
+
+local queue = function (context, extension)
+    app.queue(extension);
+end
+
+extensions = {
+    ["internal"] = {
+
+        ["*12"] = function ()
+            app.sayunixtime();
+        end;
+
+        ["_1XX"] = dial;
+
+        ["_12XX"] = queue;
+
+        ["200"] = ivr;
+
+        ["_NXXXXXX"] = outgoing_route_function;
+    };
+};
+
+hints = {};
