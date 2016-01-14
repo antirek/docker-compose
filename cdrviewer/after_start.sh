d0f4c6410c6ce9e7236cb57f1beee6abe56f4584
Sergey Dmitriev <serge.dmitriev@gmail.com>
2016-01-14 16:37:01 +0700

    fix

--- cdrviewer/after_start.sh
+++ cdrviewer/after_start.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+export VIOLA_CDR_CONFIG=/etc/violacdr/config
+
 sleep 30
+
+chmod +x /tmp/viola-cdr/app.js
 
 node /tmp/viola-cdr/app.js
---
+++ store/etc/violacdr/config.js
@@ -0,0 +1,22 @@
+
+module.exports = {
+  tz:"+0400",
+  port: 9030,
+  recordspath:"/var/records",
+  db: {
+    client: "mysql",
+    connection: {
+      host: "127.0.0.1",
+      user: "root",
+      password: "",
+      database: "cdr",
+      charset: "utf8"
+    }
+  },
+  cdr: {
+    table: "cdr"
+  },
+  auth: true,
+  username: 'admin',
+  password: 'password'
+};
